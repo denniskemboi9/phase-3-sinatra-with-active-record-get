03e1aa72c1594d29477e1b589947246620e4cfe9
DennisKemboi <cherogonydennis9@gmail.com>
2023-06-01 18:24:26 +0300

    Completed Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+  get '/games' do
+    # get all the games from the database
+    games = Game.all
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(include: :reviews)
+  end
 end
